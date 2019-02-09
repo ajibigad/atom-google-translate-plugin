f8149b12d19efca91d473be10d51613e90439235
Damilola Ajiboye <ajibigad@gmail.com>
2019-02-09 07:19:10 +0100

    initial commit

---
+++ spec/ajibigad-translate-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AjibigadTranslateView from '../lib/ajibigad-translate-view';
+
+describe('AjibigadTranslateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
